dcb6a450531e83d39e2765b8b7bfc36aa5139f7a
youssef <amerzag.youssef4@gmail.com>
2023-11-18 14:56:41 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
+
+import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
 
 function App() {
+  const imgs1 = [
+    {id : 0 , "Source" : './imgs/rock.png'},
+    {id : 1 , "Source" : './imgs/paper.png'},
+    {id : 2 , "Source" : './imgs/scissors.png'}
+]
+
+  const imgs2 = [
+    {id : 0 , "Source" : './imgs/rock.png'},
+    {id : 1 , "Source" : './imgs/paper.png'},
+    {id : 2 , "Source" : './imgs/scissors.png'}
+  ]
+
+const [choosing , setChoosing] = useState()
+const [randomId , setrandomId] = useState(0)
+const [clicking , setClicking] = useState(true)
+const [result , setResult] = useState('')
+//const [playerScore , setPlayerscore] = useState(0)
+//const [botScore , setBotscore] = useState(0)
+
+  const botchoose = useRef()
+
+  function click (id) {
+    if(clicking ===true) {
+      setChoosing(imgs1[id].id)
+      setrandomId(Math.floor(Math.random() * 3))
+      setClicking(false)
+      console.log(randomId);
+    }
+
+  }
+
+  useEffect (() => {
+    if(choosing !== undefined) {
+      if(choosing === 0 && randomId === 2 || choosing === 1 && randomId === 0 || choosing === 2 && randomId === 1) {
+        setResult('You Win!!')
+      }else if(choosing === 0 && randomId === 1 || choosing === 1 && randomId === 2 || choosing ===2 && randomId === 0) {
+        setResult('You Loose!!')
+      }else (
+        setResult('You Tied')
+      )
+      //if(result === 'You Win!!') {
+      //  setPlayerscore( playerScore + 1)
+      //}else if (result === 'You Loose!!') {
+      //  setBotscore(botScore + 1)
+      //}
+    }
+  },[click])
+
+  function again() {
+    window.location.reload()
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p id='title'>Rock Paper Scissors </p>
+      {/* <p>Player :  {playerScore} |  Computer : {botScore} </p> */}
+      <div id='imgs'>
+      {imgs1.map((img) => 
+        <img onClick={() => click(img.id)} src={img.Source} key={img.id}></img>
+      )}
+      </div>
+      <p id='result'>{result}</p>
+      <div id='botchoosing'>
+        {result ?  <img className='botchoose' ref={botchoose} src={imgs2[randomId].Source}></img>: ''}
+      </div>
+      
+
+      {result ? <button onClick={again}>Try Again</button> : ''}
     </div>
   );
 }
